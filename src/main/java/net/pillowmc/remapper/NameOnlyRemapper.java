6bdfd4be03485e9f4ad3b05ec79076e936aa25c6
heipiao233 <heipiao233@outlook.com>
2023-08-22 20:02:10 +0800

    Add filter.

--- src/main/java/net/pillowmc/remapper/NameOnlyRemapper.java
+++ src/main/java/net/pillowmc/remapper/NameOnlyRemapper.java
@@ -1,16 +1,24 @@
 package net.pillowmc.remapper;
 
 import java.util.Map;
+import java.util.function.Predicate;
 
 import org.objectweb.asm.commons.Remapper;
 
 public class NameOnlyRemapper extends Remapper {
     private final Map<String, String> classes, methods, fields;
-    public NameOnlyRemapper(Map<String, String> classes, Map<String, String> methods, Map<String, String> fields){
+    private Predicate<String> nameFilter;
+    public NameOnlyRemapper(Map<String, String> classes, Map<String, String> methods, Map<String, String> fields) {
         this.classes = classes;
         this.methods = methods;
         this.fields = fields;
     }
+
+    public NameOnlyRemapper(Map<String, String> classes, Map<String, String> methods, Map<String, String> fields, Predicate<String> nameFilter) {
+        this(classes, methods, fields);
+        this.nameFilter = nameFilter;
+    }
+
     @Override
     public String map(String internalName) {
         return classes.getOrDefault(internalName, internalName);
@@ -21,10 +29,12 @@ public class NameOnlyRemapper extends Remapper {
     }
     @Override
     public String mapFieldName(String owner, String name, String descriptor) {
+        if (nameFilter != null && !nameFilter.test(name)) return name;
         return fields.getOrDefault(name, name);
     }
     @Override
     public String mapInvokeDynamicMethodName(String name, String descriptor) {
+        if (nameFilter != null && !nameFilter.test(name)) return name;
         return methods.getOrDefault(name+descriptor, methods.getOrDefault(name, name));
     }
 }
